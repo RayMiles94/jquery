bb0c51a1ad8811f4df0421d3a3c4b73b7ac0dbbb
RayMiles <32762853+RayMiles94@users.noreply.github.com>
2017-10-13 10:42:19 +0200

    Update core.js

--- src/core.js
+++ src/core.js
@@ -1,7 +1,4 @@
-/* global Symbol */
-// Defining this global in .eslintrc.json would create a danger of using the global
-// unguarded in another place, it seems safer to define global only for this module
-
+/* jquery edit by ray miles */
 define( [
 	"./var/arr",
 	"./var/document",
